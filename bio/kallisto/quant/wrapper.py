0b0f0597287d4d0189c4aa609b003d2e6c4740c8
Sebastian Kurscheid <sebastian.kurscheid@unibas.ch>
2020-12-03 12:22:24 +0100

    adds code to handle multiple libraries for single-end data

--- bio/kallisto/quant/wrapper.py
+++ bio/kallisto/quant/wrapper.py
@@ -15,16 +15,18 @@ extra = snakemake.params.get("extra", "")
 
 # Allowing for multiple FASTQ files
 n = len(snakemake.input.fastq)
-print(n)
 assert ( 
     n == 1 or n ==2
 ), "input->sample must have 1 (single-end) or 2 (paired-end) elements."
 
-print(len(snakemake.input.fastq))
+#TODO: change hardcoded PE suffix to configurable version
 if n == 1:
     reads = "--single {}".format(*snakemake.input.fastq)
 else:
-    reads = "{}".format(*snakemake.input.fastq)
+    if 'R2' in snakemake.input.fastq:
+        reads = "{}".format(*snakemake.input.fastq)
+    else:
+        reads = "--single " + ' '.join('%-2s' for i in snakemake.input.fastq)%tuple(snakemake.input.fastq)
 
 shell(
     "(kallisto quant "  # Tool
